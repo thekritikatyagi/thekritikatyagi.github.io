d36ec9b117276bcb224ba7f45c595b39cae7bb83
Kritika Tyagi <thekritikatyagi@hotmail.com>
2025-02-18 02:03:59 +0530

    Update build.py

--- build.py
+++ build.py
@@ -100,7 +100,7 @@ def make_pdf(config, data):
     """
     from weasyprint import HTML
     output_dir = config.get('output_dir', 'build')
-    output_file = os.path.join(output_dir, config.get('pdf_file', 'resume.pdf'))
+    output_file = os.path.join(output_dir, config.get('pdf_file', 'Kritikatyagi.pdf'))
     input_file = os.path.join(output_dir, 'index.html')
     theme_location = os.path.join('themes', config['theme'])
     html = HTML(input_file, base_url=theme_location)
